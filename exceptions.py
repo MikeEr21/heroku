ecf78510115b43d6990f31bbc7ab77d175d50bf8
MikeEr21 <77528007+MikeEr21@users.noreply.github.com>
2022-10-04 19:26:59 +0300

    Add new file

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+class GetAPIAnswerError(Exception):
+    pass
+
+
+class CacheTokenError(Exception):
+    pass
+
+
+class ParseStatusError(Exception):
+    pass
+
+
+class CheckResponseError(Exception):
+    pass
+
+
