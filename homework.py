6510291e5e7dc0b522a7db0b1b19a8b5143e83f0
MikeEr21 <77528007+MikeEr21@users.noreply.github.com>
2022-10-08 01:29:31 +0300

    After second review

--- homework.py
+++ homework.py
@@ -44,42 +44,59 @@ def send_message(bot: Bot, message: str) -> None:
     try:
         logging.info(f'Сообщение "{message}" готовится к отправке.')
         bot.send_message(TELEGRAM_CHAT_ID, message)
-    except telegram.TelegramError:
+    except telegram.TelegramError as error:
         logging.error(
-            f'Невозможно отправить сообщение.'
+            f'Невозможно отправить сообщение. {error}'
         )
     else:
-        logging.info(f'Сообщение успешно отправлено.')
+        logging.info('Сообщение успешно отправлено.')
 
 
 def get_api_answer(current_timestamp: int) -> dict:
     """Делаем запрос к эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    logging.info('Делаем запрос к эндпоинту API-сервиса.')
-    homework_statuses = requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params=params
-    )
-    if homework_statuses.status_code != HTTPStatus.OK:
-        raise GetAPIAnswerError()
-    return homework_statuses.json()
+    try:
+        logging.info('Делаем запрос к эндпоинту API-сервиса.')
+        homework_statuses = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+        if homework_statuses.status_code != HTTPStatus.OK:
+            raise GetAPIAnswerError(
+                f'Ошибка {homework_statuses.status_code} '
+                f'по адресу {ENDPOINT}, где заголовок {HEADERS}'
+            )
+    except AttributeError as error:
+        raise GetAPIAnswerError(
+            f'Что-то не так с ссылкой {ENDPOINT}. {error}'
+        )
+    else:
+        return homework_statuses.json()
 
 
 def check_response(response: dict) -> dict:
     """Проверка ответа API на корректность."""
     logging.info('Проверяем ответ API на корректность.')
+    if not isinstance(response, dict):
+        raise TypeError(
+            f'Ответ от API имеет некорректный '
+            f'тип {isinstance(response, dict)}'
+        )
+    if 'homeworks' not in response:
+        raise CheckResponseError('В ответе от API нет ключа "homeworks"')
+    if not isinstance(response['homeworks'], list):
+        raise CheckResponseError(
+            f'По ключу "homeworks" возвращается '
+            f'не список {isinstance(response["homeworks"], list)} '
+        )
+    if response['homeworks'] is []:
+        raise CheckResponseError(
+            'В данный момент нет домашних заданий на проверке'
+        )
     homeworks = response['homeworks']
     homework = homeworks[0]
-    if response != HTTPStatus.OK:
-        logging.error('API не отвечает')
-    if type(homework) != dict:
-        logging.error('Ответ от API имеет некорректный тип')
-        raise CheckResponseError
-    if homework == '':
-        logging.error('В списке нет домашних заданий')
-        raise CheckResponseError
     return homework
 
 
@@ -87,19 +104,15 @@ def parse_status(homework: dict) -> str:
     """Извлекаем информацию о статусе домашней работы."""
     logging.info('Извлекаем информацию о статусе домашней работы.')
     if 'homework_name' not in homework:
-        logging.error('В списке нет домашних заданий')
-        raise KeyError()
+        raise KeyError('В списке нет домашних заданий')
     homework_name = homework['homework_name']
     if homework['status'] not in HOMEWORK_STATUSES.keys():
-        logging.error('Отсутствует статус ревью')
-        raise ParseStatusError
+        raise ParseStatusError('Отсутствует статус ревью')
     if homework_name not in homework['homework_name']:
-        logging.error('Отсутствует название работы')
-        raise ParseStatusError
+        raise ParseStatusError('Отсутствует название работы')
     verdict = HOMEWORK_STATUSES.get(homework['status'])
     if verdict is None:
-        logging.error('Пустой verdict')
-        raise ParseStatusError
+        raise ParseStatusError('Пустой verdict')
     return (
         f'Изменился статус проверки работы '
         f'"{homework_name}". {verdict}'
@@ -109,32 +122,33 @@ def parse_status(homework: dict) -> str:
 def check_tokens() -> bool:
     """Проверяем наличие переменных окружения."""
     logging.info('Проверяем наличие переменных окружения.')
-    values = (
-        ('PRACTICUM_TOKEN', PRACTICUM_TOKEN),
-        ('TELEGRAM_TOKEN', TELEGRAM_TOKEN),
-        ('TELEGRAM_CHAT_ID', TELEGRAM_CHAT_ID)
-    )
-    empty_variables = []
-    for variable_name, variable_value in values:
-        if variable_value is None:
-            empty_variables.append(variable_name)
-            logging.critical(f'У объекта/-ов "{empty_variables[0]}" нет переменной окружения.')
-            return False
-    return True
+    if PRACTICUM_TOKEN is None:
+        logging.critical(
+            f'У объекта/-ов "{"PRACTICUM_TOKEN"}" нет переменной окружения.'
+        )
+    if TELEGRAM_TOKEN is None:
+        logging.critical(
+            f'У объекта/-ов "{"TELEGRAM_TOKEN"}" нет переменной окружения.'
+        )
+    if TELEGRAM_CHAT_ID is None:
+        logging.critical(
+            f'У объекта/-ов "{"TELEGRAM_CHAT_ID"}" нет переменной окружения.'
+        )
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 class GetAPIAnswerError(Exception):
-    def __init__(self):
-        self.message = 'API не отвечает.'
+    def __init__(self, error):
+        self.message = f'API не отвечает. {error}'
 
     def __str__(self):
         return self.message
 
 
 class ParseStatusError(Exception):
-    def __init__(self):
+    def __init__(self, error):
         self.message = (
-            'Есть проблемы с извлекаемой информацией из статуса работы.'
+            f'Есть проблемы с извлекаемой информацией из статуса работы. {error}'
         )
 
     def __str__(self):
@@ -142,35 +156,40 @@ class ParseStatusError(Exception):
 
 
 class CheckResponseError(Exception):
-    def __init__(self):
+    def __init__(self, error):
         self.message = (
-            """При проверке ответа API
-            на корректность возникли проблемы."""
+            f'При проверке ответа API '
+            f'на корректность возникли проблемы. {error}'
         )
 
     def __str__(self):
         return self.message
 
 
+
 def main() -> None:
     """Запуск бота."""
-    """Основная логика работы бота."""
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     last_message = None
     last_error = None
 
-    while check_tokens():
+    while True:
         try:
-            response = get_api_answer(current_timestamp)
-            homework = check_response(response)
-            message = parse_status(homework)
+            if check_tokens() is False:
+                sys.exit('Потерялись переменные окружения')
+            else:
+                response = get_api_answer(current_timestamp)
+                homework = check_response(response)
+                message = parse_status(homework)
+
             if homework is not None and message != last_message:
                 send_message(bot, message)
                 last_message = message
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            logging.info(message)
             if message != last_error:
                 send_message(bot, message)
                 last_error = message
